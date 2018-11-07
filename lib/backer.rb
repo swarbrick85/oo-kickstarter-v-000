59315d8e3c45ffa38824bc24fc117b157f69ae7b
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-07 16:30:10 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,12 @@
 class Backer 
   
-  attr_reader :name, :projects 
+  attr_reader :name 
+  attr_accessor :backed_projects 
   
   
   def initialize(name)
     @name = name 
-    @projects = []
+    @backed_projects = []
   end 
   
   
